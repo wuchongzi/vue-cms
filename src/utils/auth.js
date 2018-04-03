0d979209f2e378d3ea0967479ef402a9c95c1658
Chosen wu <wuchong820@163.com>
2018-04-03 10:47:58 +0800

    token浏览器过期时间设置

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -6,18 +6,18 @@ import Cookies from "js-cookie";
 // 设置键名
 const TokenKey = "BANGPAY_E_AUTH";
 
-// 设置浏览器过期时间 - 60分钟
-// const outTime = 1 / 24;
+// 设置浏览器过期时间 - 6小时
+const outTime = 6 / 24;
 
 export default {
     getToken: () => {
         return Cookies.get(TokenKey) || "";
     },
     setToken: (token) => {
-        // return Cookies.set(TokenKey, token, {
-        //     expires: outTime
-        // });
-        return Cookies.set(TokenKey, token);
+        return Cookies.set(TokenKey, token, {
+            expires: outTime
+        });
+        // return Cookies.set(TokenKey, token);
     },
     removeToken: () => {
         return Cookies.remove(TokenKey)
